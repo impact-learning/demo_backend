868fe5eb7a99097b7bb3e8deb7aebad45f07db09
Terrence Lam <skyuplam@gmail.com>
2016-06-29 16:38:38 +0800

    Minor changes

--- demo_backend/api.py
+++ demo_backend/api.py
@@ -5,7 +5,7 @@ from flask_socketio import emit
 from demo_backend.models import db
 import rethinkdb as r
 from demo_backend.settings import ProdConfig
-from demo_backend.modeling import get_data_dict
+from demo_backend.modeling import preprocessing_data
 
 
 @socketio.on('my event')
@@ -16,13 +16,12 @@ def test_message(message):
 
 @socketio.on('search county')
 def handle_search(county):
-    # conn = r.connect(ProdConfig.DB_HOST, ProdConfig.DB_PORT)
-    # cursor = db.table('township_village').pluck(['county', 'township', 'village', 'coordinates', 'scores', 'average_income_per_capita', 'number_of_trees']).run(conn)
-    # villages = []
-    # for item in cursor:
-    #     villages.append(item)
-    # emit('search response', villages)
-    #
-    # conn.close()
-    get_data()
+    conn = r.connect(ProdConfig.DB_HOST, ProdConfig.DB_PORT)
+    cursor = db.table('township_village').pluck(['county', 'township', 'village', 'coordinates', 'scores', 'average_income_per_capita', 'number_of_trees']).run(conn)
+    villages = []
+    for item in cursor:
+        villages.append(item)
+    emit('search response', villages)
+
+    conn.close()
     print 'Handle Serach'
